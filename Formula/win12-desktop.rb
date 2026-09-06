43b00052974080f093f7dc4134536949dcd93584
freedom-323 <210706743+freedom-323@users.noreply.github.com>
2026-09-06 16:22:22 +0800

    小修小补

--- Formula/win12-desktop.rb
+++ Formula/win12-desktop.rb
@@ -52,6 +52,6 @@ class Win12Desktop < Formula
   end
 
   livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    url "https://github.com/win12-online/win12-desktop"
+    strategy :github_latest
   end
